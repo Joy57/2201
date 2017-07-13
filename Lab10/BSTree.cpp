314ed8f08c022d96073f0021ec5375c8c217f492
Joy57 <abedinjoynal2@gmail.com>
2017-07-12 18:23:36 -0700

    Add files via upload

---
+++ Lab10/BSTree.cpp
@@ -0,0 +1,251 @@
+/*Joynal Abedin*/
+#include "BSTree.h"
+#include <algorithm>
+
+template <typename DataType, class KeyType>
+void BSTree<DataType, KeyType>::copyTree(const BSTree<DataType, KeyType> &otherTree)
+{
+	copyTreeHelper(root, otherTree.root);
+}
+template <typename DataType, class KeyType>
+void BSTree<DataType, KeyType>::copyTreeHelper(BSTreeNode *&p, const BSTreeNode *otherPtr)
+{
+	if (p != 0)
+	{
+		p = new BSTreeNode(otherPtr->dataItem, 0, 0);
+		copyTreeHelper(p->left, otherPtr->left)
+			copyTreeHelper(p->right, otherPtr->right);
+	}
+
+}
+
+template <typename DataType, class KeyType>
+BSTree<DataType, KeyType>::BSTreeNode::BSTreeNode(const DataType &nodeDataItem, BSTreeNode *leftPtr, BSTreeNode *rightPtr)
+{
+	dataItem = nodeDataItem;
+	left = leftPtr;
+	right = rightPtr;
+}
+
+template < typename DataType, class KeyType >
+BSTree<DataType, KeyType>::BSTree()
+{
+	root = NULL;
+}
+
+template < typename DataType, class KeyType >
+BSTree<DataType, KeyType>::BSTree(const BSTree<DataType, KeyType>& other)
+{
+	*this = other;
+}
+
+template < typename DataType, class KeyType >
+BSTree<DataType, KeyType>& BSTree<DataType, KeyType>:: operator= (const BSTree<DataType, KeyType>& other)
+{
+	if (*this == &other)
+		return *this;
+	clear();
+	copyTreeHelper(root, other.root);
+	return *this;
+}
+
+template < typename DataType, class KeyType >
+BSTree<DataType, KeyType>::~BSTree()
+{
+	clearHelper(root);
+}
+
+template <typename DataType, class KeyType>
+void BSTree<DataType, KeyType>::clearHelper(BSTreeNode * &p)
+{
+	if (p != nullptr)
+	{
+		clearHelper(p->left);
+		clearHelper(p->right);
+		delete p;
+	}
+	p = nullptr;
+}
+
+template < typename DataType, class KeyType >
+void BSTree<DataType, KeyType>::insert(const DataType& newDataItem)
+{
+	insertHelper(root, newDataItem);
+}
+
+
+template < typename DataType, class KeyType >
+void BSTree<DataType, KeyType>::insertHelper(BSTreeNode *&p, const DataType &newDataItem)
+{
+	if (p == nullptr)
+		p = new BSTreeNode{ newDataItem, nullptr, nullptr };
+	else if (newDataItem.getKey() < p->dataItem.getKey())
+	{
+		insertHelper(p->left, newDataItem);
+	}
+	else if (newDataItem.getKey() > p->dataItem.getKey())
+	{
+		insertHelper(p->right, newDataItem);
+	}
+	else; //duplicate, do nothing
+}
+
+
+template < typename DataType, class KeyType >
+bool BSTree<DataType, KeyType>::retrieve(const KeyType& searchKey, DataType& searchDataItem) const
+{
+
+	return retrieveHelper(root, searchKey, searchDataItem);
+
+}
+
+
+template < typename DataType, class KeyType >
+bool BSTree<DataType, KeyType>::retrieveHelper(BSTreeNode *p, const KeyType& searchKey, DataType &searchDataItem) const
+{
+	if (p == NULL)
+		return NULL;
+	else if (searchKey < p->dataItem.getKey())
+		return retrieveHelper(p->left, searchKey, searchDataItem);
+	else if (searchKey > p->dataItem.getKey())
+		return retrieveHelper(p->right, searchKey, searchDataItem);
+	else
+	{
+		searchDataItem = p->dataItem;
+		return true; //match
+	}
+
+}
+
+
+template < typename DataType, class KeyType >
+bool BSTree<DataType, KeyType>::remove(const KeyType& deleteKey)
+{
+
+	return removeHelper(root, deleteKey);
+}
+template < typename DataType, class KeyType >
+bool BSTree<DataType, KeyType>::removeHelper(BSTreeNode *&p, const KeyType& deleteKey)
+{
+	if (p == nullptr)
+		return false;
+	else if (deleteKey < p->dataItem.getKey())
+	{
+		return removeHelper(p->left, deleteKey);
+	}
+	else if (deleteKey > p->dataItem.getKey())
+	{
+		return removeHelper(p->right, deleteKey);
+	}
+	else{
+		BSTreeNode *oldNode = p;
+		if (p->left == nullptr)
+			p = p->right;
+		else if (p->right == nullptr)
+			p = p->left;
+		else
+			cutRightmost(p->left, oldNode);
+		delete oldNode;
+		return true;
+	}
+
+}
+
+
+template < typename DataType, class KeyType >
+void BSTree<DataType, KeyType>::cutRightmost(BSTreeNode *&r, BSTreeNode *&delPtr)
+{
+	if (r->right != nullptr)
+		cutRightmost(r->right, delPtr);
+	else
+	{
+		delPtr->dataItem = r->dataItem;
+		delPtr = r;
+		r = r->left;
+	}
+}
+
+template < typename DataType, class KeyType >
+void BSTree<DataType, KeyType>::writeKeys() const
+{
+	writeKeysHelper(root);
+}
+
+template < typename DataType, class KeyType >
+void BSTree<DataType, KeyType>::writeKeysHelper(BSTreeNode *p) const
+{
+	if (p != nullptr)
+	{
+
+		writeKeysHelper(p->left);
+		cout << p->dataItem.getKey() << " "; //line separated by spaces
+		writeKeysHelper(p->right);
+	}
+	return;
+}
+
+
+template < typename DataType, class KeyType >
+void BSTree<DataType, KeyType>::clear()
+{
+	clearHelper(root);
+}
+
+template < typename DataType, class KeyType >
+bool BSTree<DataType, KeyType>::isEmpty() const
+{
+	return (root == nullptr);
+}
+
+template < typename DataType, class KeyType >
+int BSTree<DataType, KeyType>::getHeight() const
+{
+	return getHeightHelper(root) - 1;
+}
+
+template < typename DataType, class KeyType >
+int BSTree<DataType, KeyType>::getHeightHelper(BSTreeNode *p) const
+{
+	if (p != nullptr)
+	{
+		return max(getHeightHelper(p->left), getHeightHelper(p->right)) + 1;
+	}
+	else
+		return 0;
+}
+
+template < typename DataType, class KeyType >
+int BSTree<DataType, KeyType>::getCount() const
+{
+	return getCountHelper(root);
+}
+
+template < typename DataType, class KeyType >
+int BSTree<DataType, KeyType>::getCountHelper(BSTreeNode *p) const
+{
+	if (p == nullptr)
+		return 0;
+	else
+		return 1 + getCountHelper(p->left) + getCountHelper(p->right);
+}
+
+template < typename DataType, class KeyType >
+void BSTree<DataType, KeyType>::writeLessThan(const KeyType& searchKey) const
+{
+	writeLTHelper(root, searchKey);
+}
+
+template < typename DataType, class KeyType >
+void BSTree<DataType, KeyType>::writeLTHelper(BSTreeNode *p, const KeyType& searchKey) const
+{
+	if (p == nullptr)
+		return;
+	writeLTHelper(p->left, searchKey);
+	if (p->dataItem.getKey() < searchKey)
+	{
+		cout << p->dataItem.getKey() << " ";
+		writeLTHelper(p->right, searchKey);
+	}
+}
+
+#include "show9.cpp"
---
+++ Lab10/HashTable.cpp
@@ -0,0 +1,106 @@
+
+#include "HashTable.h"
+
+template <typename DataType, typename KeyType>
+HashTable<DataType, KeyType>::HashTable(int initTableSize) :tableSize(initTableSize)
+{
+	dataTable = new BSTree<DataType, KeyType>[tableSize];
+}
+
+template <typename DataType, typename KeyType>
+HashTable<DataType, KeyType>::HashTable(const HashTable& other)
+{
+	tableSize = other.tableSize;
+	dataTable = new BSTree<DataType, KeyType>[tableSize];
+	copyTable(other);
+}
+
+template <typename DataType, typename KeyType>
+HashTable<DataType,KeyType>& HashTable<DataType, KeyType>::operator=(const HashTable& other)
+{
+	clear();
+	copyTable(other);
+}
+
+template <typename DataType, typename KeyType>
+HashTable<DataType, KeyType>::~HashTable()
+{
+	clear();
+}
+
+template <typename DataType, typename KeyType>
+void HashTable<DataType, KeyType>::insert(const DataType& newDataItem)
+{
+	unsigned int loc = DataType::hash(newDataItem.getKey()) % tableSize;
+	dataTable[loc].insert(newDataItem);
+}
+
+template <typename DataType, typename KeyType>
+bool HashTable<DataType, KeyType>::remove(const KeyType& deleteKey)
+{
+	unsigned int loc = DataType::hash(deleteKey) % tableSize;
+	return dataTable[loc].remove(deleteKey);
+}
+
+template <typename DataType, typename KeyType>
+bool HashTable<DataType, KeyType>::retrieve(const KeyType& searchKey, DataType& returnItem) const
+{
+	unsigned int loc = DataType::hash(searchKey) % tableSize;
+	return dataTable[loc].retrieve(searchKey, returnItem);
+}
+
+template <typename DataType, typename KeyType>
+void HashTable<DataType, KeyType>::clear()
+{
+	for (int i = 0; i < tableSize; i++)
+	{
+		dataTable[i].clear();
+	}
+}
+
+template <typename DataType, typename KeyType>
+bool HashTable<DataType, KeyType>::isEmpty() const
+{
+	for (int i = 0; i < tableSize; i++)
+	{
+		if (!dataTable[i].isEmpty())
+			return false;
+	}
+	return true;
+}
+
+#include "show10.cpp"
+
+template <typename DataType, typename KeyType>
+double HashTable<DataType, KeyType>::standardDeviation() const
+{
+	int t = 0;
+	double sum = 0;
+
+	for (int i = 0; i < tableSize; i++)
+	{
+		t += dataTable[i].getCount();
+	}
+	double load = t / tableSize; 
+	for (int i = 0; i < tableSize; i++)
+	{
+		int n = dataTable.getCount();
+		sum += (n - load) *(n - load);
+	}
+	return sqrt(sum / (tableSize - 1));
+
+}
+
+template <typename DataType, typename KeyType>
+void HashTable<DataType, KeyType>::copyTable(const HashTable& source)
+{
+	delete dataTable; //get rid of the old datatable
+
+	
+	dataTable = new BSTree<DataType, KeyType>[source.table];
+
+	//copy every element
+	for (int i = 0; i < source.tableSize; i++)
+		dataTable[i] = source.dataTable[i];
+
+}
